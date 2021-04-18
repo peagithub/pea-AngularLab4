a6adb933fc282fce8ea329a8004b0e497ae1e86e
peagithub <81603451+peagithub@users.noreply.github.com>
2021-04-18 15:48:51 +0700

    Add files via upload

---
+++ lab4.component.ts
@@ -0,0 +1,12 @@
+import { Component,  } from '@angular/core';
+@Component({
+selector: 'app-Lab4Component',
+templateUrl: './lab4.component.html',
+styleUrls : ['./lab4.component.css']
+
+})
+export class Lab4Component {
+    items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+constructor() {}
+}
